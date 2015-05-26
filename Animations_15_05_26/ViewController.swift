691d0dea8cbc9e0d139c6e0f3a79c729bd5a040f
Chris Wardman <chrisatdarley@aol.com>
2015-05-26 15:47:49 +0100

    Initial Commit

---
+++ Animations_15_05_26/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations_15_05_26
+//
+//  Created by Chris Wardman on 26/05/2015.
+//  Copyright (c) 2015 Chris Wardman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
