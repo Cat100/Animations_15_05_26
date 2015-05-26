a877f827b81d48cc06314ad40bf8dc0a451b6426
Chris Wardman <chrisatdarley@aol.com>
2015-05-26 15:57:24 +0100

    rev01

---
+++ CST_FirstAnimation.swift
@@ -0,0 +1,14 @@
+//
+//  CST_FirstAnimation.swift
+//  Animations_15_05_26
+//
+//  Created by Chris Wardman on 26/05/2015.
+//  Copyright (c) 2015 Chris Wardman. All rights reserved.
+//
+
+import UIKit
+
+class CST_FirstAnimation: NSObject {
+    
+    //
+}
