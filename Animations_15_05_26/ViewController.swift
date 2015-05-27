4d3f626c36fb3d94349e06457e9ea2dc005cab23
Chris Wardman <chrisatdarley@aol.com>
2015-05-27 13:05:03 +0100

    issues still with 2nd view shifting before animation

--- Animations_15_05_26/ViewController.swift
+++ Animations_15_05_26/ViewController.swift
@@ -10,27 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var testView: UIView!
+    var testView2: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let testView: UIView = UIView(frame: CGRectMake(20, 100, 20, 20))
+        testView = UIView(frame: CGRectMake(20, 100, 20, 20))
+        testView2 = UIView(frame: CGRectMake(20, 140, 20, 20))
         
         view.addSubview(testView)
+        view.addSubview(testView2)
         
         let image: UIImage = UIImage(named: "stick1.png")!
         let imageView: UIImageView = UIImageView(image: image)
         
+        let image2: UIImage = UIImage(named: "stick1.png")!
+        let imageView2: UIImageView = UIImageView(image: image2)
+        
         testView.addSubview(imageView)
+        testView.layer.position = CGPointMake(20, 100)
+        
+        testView2.addSubview(imageView2)
+        testView2.layer.position = CGPointMake(20, 140)
+
+
+    }
+    
+    @IBAction func button1(sender: AnyObject) {
         
         var animation: CABasicAnimation = CABasicAnimation()
-            animation.keyPath = "position.x"
-            animation.fromValue = 20
-            animation.toValue = 280
-            animation.duration = 3
+        animation.keyPath = "position.x"
+        animation.fromValue = 20
+        animation.byValue = 180
+        animation.duration = 3
         
         testView.layer.addAnimation(animation, forKey: "basic")
+        testView.layer.position = CGPointMake(200, 100)
         
-        testView.layer.position = CGPointMake(280, 100)
+        animation.beginTime = CACurrentMediaTime() + 0.5
+        
+        testView2.layer.addAnimation(animation, forKey: "basic")
+        testView2.layer.position = CGPointMake(20, 140)
+        testView2.layer.position = CGPointMake(200, 140)
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
